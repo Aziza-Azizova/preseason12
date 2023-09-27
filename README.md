acfeca94072edd17d5e2ffef3b2aff2f8a5e4c6e
Aziza-Azizova <aziza.azizova01@gmail.com>
2023-09-27 12:46:59 +0500

    Added function and some paragraph to HTML

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Git && GitHub Lesson
 
 Preseason 12 second group
+
+# Installation
+
+install express with npm i express
--- script.js
+++ script.js
@@ -3,3 +3,7 @@ console.log("Hello Git World!");
 for(let i=0; i<=10; i++){
     console.log(i);
 }
+
+function greeting(){
+    alert("wqertyuio")
+}
